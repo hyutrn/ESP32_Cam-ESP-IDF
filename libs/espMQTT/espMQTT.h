5b07565c1efb1af4ee2317c41046fae8d969f234
TrongHuy <115525478+hyutrn@users.noreply.github.com>
2024-06-03 14:54:47 +0700

    Create espMQTT.h

---
+++ libs/espMQTT/espMQTT.h
@@ -0,0 +1,33 @@
+#ifndef _ESP_MQTT_H_
+#define _ESP_MQTT_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+//#include "protocol_examples_common.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+#include "espWiFi.h"
+#include "espHTTP.h"
+#include "espCLIENT.h"
+
+esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event);
+void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
+void mqtt_app_start(void);
+#endif 
